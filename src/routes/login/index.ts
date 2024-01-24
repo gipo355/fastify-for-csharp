a0a6dc271a7e87d09b952f6e5c265d44f2a33e00
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-01-24 16:15:23 +0100

    done

--- src/routes/login/index.ts
+++ src/routes/login/index.ts
@@ -5,35 +5,35 @@ import { v4 as uuidv4 } from "uuid";
 import { prisma } from "../../prisma.js";
 
 const root: FastifyPluginAsync = async (fastify): Promise<void> => {
-  fastify.post<{ Body: { email: string; password: string } }>(
-    "/",
-    async function (request) {
-      const { email, password } = request.body;
+    fastify.post<{ Body: { email: string; password: string } }>(
+        "/",
+        async function (request) {
+            const { email, password } = request.body;
 
-      const user = await prisma.user.findUnique({
-        where: {
-          email,
-          password,
+            const user = await prisma.user.findUnique({
+                where: {
+                    email,
+                    password,
+                },
+            });
+
+            if (!user) {
+                throw new Error("Invalid email or password");
+            }
+
+            const updatedUser = await prisma.user.update({
+                where: {
+                    id: user.id,
+                },
+                data: {
+                    token: uuidv4(),
+                    updatedAt: new Date(),
+                },
+            });
+
+            return updatedUser;
         },
-      });
-
-      if (!user) {
-        throw new Error("Invalid email or password");
-      }
-
-      await prisma.user.update({
-        where: {
-          id: user.id,
-        },
-        data: {
-          token: uuidv4(),
-          updatedAt: new Date(),
-        },
-      });
-
-      return user;
-    },
-  );
+    );
 };
 
 export default root;
